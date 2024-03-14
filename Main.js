1ca18977aa8882a81b53cfca27f0e263453d2091
Flappy <stvnsbarrantes@gmail.com>
2024-03-14 18:29:39 +0800

    Start

--- Main.js
+++ Main.js
@@ -4,11 +4,11 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-// create a component
+
 function MyComponent() {
     return (
         // <View style={styles.container}>
-            <Text>MyComponent</Text>
+            <Text >Hello</Text>
         // </View>
     );
 };
