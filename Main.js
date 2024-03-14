28aa29320ddafdfb49603b6d00b7a03912d954ae
Flappy <stvnsbarrantes@gmail.com>
2024-03-14 17:23:39 +0800

    Second Push

---
+++ Main.js
@@ -0,0 +1,26 @@
+
+
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+// create a component
+function MyComponent() {
+    return (
+        // <View style={styles.container}>
+            <Text>MyComponent</Text>
+        // </View>
+    );
+};
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         backgroundColor: '#2c3e50',
+//     },
+// });
+
+
+export default MyComponent;
