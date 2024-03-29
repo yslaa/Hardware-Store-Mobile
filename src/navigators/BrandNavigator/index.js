af93b64eddc7fd0112ed883ac54169056b87fd63
yslaa <crislhan.fabila@tup.edu.ph>
2024-03-29 20:08:16 +0800

    Add BrandUpdate screen to BrandNavigator

--- src/navigators/BrandNavigator/index.js
+++ src/navigators/BrandNavigator/index.js
@@ -2,6 +2,7 @@ import { createStackNavigator } from '@react-navigation/stack'
 import Brand from '@screens/Admin/Brand'
 // import BrandTest from '@screens/Admin/BrandTest'
 import BrandForm from '@screens/Admin/BrandForm'
+import BrandUpdate from '@screens/Admin/BrandUpdate'
 import React from 'react'
 
 const Stack = createStackNavigator()
@@ -16,7 +17,7 @@ const index = () => {
         }}
         />
         <Stack.Screen name="BrandForm" component={BrandForm}/>
-        {/* <Stack.Screen name="BrandTest" component={BrandTest}/> */}
+        <Stack.Screen name="BrandUpdate" component={BrandUpdate}/>
     </Stack.Navigator>
   )
 }
