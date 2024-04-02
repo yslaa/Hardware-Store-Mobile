63ace050974f303a088b2fa8eac063e6d6a1cd76
reden21345 <mirandilla395@gmail.com>
2024-04-02 22:36:14 +0800

    baseUrl Deployment url

--- assets/commons/baseurl.js
+++ assets/commons/baseurl.js
@@ -4,8 +4,8 @@ import { Platform } from 'react-native'
 let baseURL = '';
 
 {Platform.OS == 'android'
-? baseURL = 'http://192.168.67.195:4000/api/v1/'
-: baseURL = 'http://192.168.67.195:4000/api/v1/'
+? baseURL = 'https://fixit-api.onrender.com/api/v1/'
+: baseURL = 'https://fixit-api.onrender.com/api/v1/'
 }
 
 export default baseURL;
